6ec5c33cfddd80065165e1b39c87d29503a64278
ganbold-adilbish <ganbold.adilbish.111@gmail.com>
2024-07-20 14:09:33 +0800

    Add CandleStickChart

---
+++ components/CandlestickChart/index.tsx
@@ -0,0 +1,56 @@
+"use client";
+import dynamic from "next/dynamic";
+import { ApexOptions } from "apexcharts";
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
+
+type SeriesDataItem = {
+  x: string;
+  y: string[];
+};
+
+type CandleStickChartProps = {
+  seriesData: SeriesDataItem[];
+  titleText: string;
+};
+
+export default function CandleStickChart({
+  seriesData,
+  titleText,
+}: CandleStickChartProps) {
+  const series = [
+    {
+      data: seriesData,
+    },
+  ];
+  const options: ApexOptions = {
+    chart: {
+      type: "candlestick",
+    },
+    title: {
+      text: titleText,
+      align: "left",
+    },
+    xaxis: {
+      type: "datetime",
+    },
+    yaxis: {
+      tooltip: {
+        enabled: true,
+      },
+    },
+  };
+
+  return (
+    <div className="w-[80%]">
+      <ReactApexChart
+        options={options}
+        series={series}
+        type="candlestick"
+        height={450}
+        width={"100%"}
+      />
+    </div>
+  );
+}
